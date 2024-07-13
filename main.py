92cf7d2b1da9b6012d0da4c67ab420a144fd6d2f
Galaktionofff <galaktionofff@gmail.com>
2024-07-13 15:28:17 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#1st program
+print(9 ** 0.5 * 5)
+
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program
+
+x = 1234
+y = 5678
+x = x % 1000
+x = x // 10
+y = y % 5000
+y = y // 10
+print(x + y)
+
+#4th program
+x = 13.42
+y = 42.13
+x = int(x * 100)
+y = int(y * 100)
+x1 = x % 1300
+y1 = y % 4200
+x = x // 100
+y = y // 100
+print(x == y1 or y == x1)
