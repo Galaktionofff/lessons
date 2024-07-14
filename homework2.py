380df342aeda212be5c9a4324f7133079e42fc1b
Galaktionofff <galaktionofff@gmail.com>
2024-07-14 13:26:10 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+HM_count = 12
+H_count = 1.5
+Name = 'Python'
+time = (H_count / HM_count)
+print('Курс:', Name, ',всего задач:', HM_count, ",затрачено часов:", H_count, ",среднее время выполнения", time, "часа")
