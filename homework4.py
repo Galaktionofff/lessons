c3db433f0dc420dc886e0f0ead560105423d594b
Galaktionofff <galaktionofff@gmail.com>
2024-07-14 14:42:35 +0500

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,6 @@
+my_srting = input("Введите произвольное значение: ")
+print(my_srting .upper())
+print(my_srting .lower())
+print(my_srting .replace(' ', ''))
+print(my_srting [0])
+print(my_srting [-1])
