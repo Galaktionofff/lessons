c8705b6c35d79ac2621e29519a732f7ea73a1e9f
Galaktionofff <galaktionofff@gmail.com>
2024-07-28 17:09:25 +0500

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,11 @@
+s = 0
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+while (my_list[s]) >= 0:
+    if my_list[s] > 0:
+        print(my_list[s])
+        s = s + 1
+    elif my_list[s] == 0:
+        s = s + 1
+        continue
+    else:
+        break
