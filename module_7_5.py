fdfb16a8777ae27a07a478ed945b8e9c81009ffb
Galaktionofff <galaktionofff@gmail.com>
2024-10-05 17:52:45 +0500

    Update module_7_5.py

--- module_7_5.py
+++ module_7_5.py
@@ -1,8 +1,11 @@
-directory = os.getcwd()
-for i in os.walk('.'):
-    print(i)
-print(os.path.join('/Users/nikitagalaktionov/PycharmProjects/Urban/NameSpace/module_7/module_7_2.py'))
-print(time.ctime(os.path.getmtime('module_7_1.py')))
-print(os.path.getsize('module_7_1.py'))
-print(os.path.dirname('/Users/nikitagalaktionov/PycharmProjects/Urban/NameSpace/module_7/module_7_1.py'))
-
+for root, dirs, files in os.walk('.'):
+    for file in files:
+        filepath = os.path.join(file)
+        filetime = os.stat(file).st_ctime #Выводит информацию о последнем изменении файла в секундах
+        filetime = time.localtime(filetime) # Подгоняет значение под формат даты и времени
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", filetime) # Подгоняет значение под необходимый формат
+        filesize = os.path.getsize(file)
+        absfile = os.path.abspath(file)# Возвращает абсолютный путь к файлу
+        parent_dir = os.path.dirname(absfile)
+        print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize}'
+                f' байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
