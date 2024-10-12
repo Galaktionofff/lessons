93ab5c346782026b8683f0242c2cfc5636858ba6
Galaktionofff <galaktionofff@gmail.com>
2024-10-12 22:28:30 +0500

    Update module_8_2.py

--- module_8_2.py
+++ module_8_2.py
@@ -1,31 +1,32 @@
-class TypeError(Exception):
-    pass
+s = 0
 def personal_sum(*numbers):
+    global s
     result = 0
     incorrect_data = 0
-    for i in range (len(numbers)):
-        if not isinstance(numbers, list):
-            raise TypeError
-        try:
-            result += numbers[i]
-        except(TypeError):
-            incorrect_data += 1
-        return  (result, incorrect_data)
-
+    if isinstance(numbers[0], list):
+        for i in range(len(numbers[0])):
+            try:
+                result += numbers[0][i]
+                s += 1
+            except(TypeError):
+                incorrect_data += 1
+                print(f'Некорректный тип данных для подсчёта суммы - {numbers[0][i]}')
+                continue
+    else:
+        raise TypeError
+    return (result, incorrect_data)
 def calculate_average(*numbers):
+    global s
     try:
         result = personal_sum(*numbers)
-        sum1 = 0
-        for i in range(len(numbers)):
-            if isinstance(numbers[i], int):
-                sum1 +=1
-            else:
-                continue
-        sa = result[0] / sum1
+        sa = result[0] / s
+        s = 0
         return sa
     except(ZeroDivisionError):
-        return 0
+        s = 0
+        return None
     except(TypeError):
+        s = 0
         print('В numbers записан некорректный тип данных')
         return None
 
@@ -34,3 +35,4 @@ print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка п
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
 print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
 print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
+
