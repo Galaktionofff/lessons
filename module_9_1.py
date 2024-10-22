6dc75bed6c258366797c4a8fd4cf9e80a9932514
Galaktionofff <galaktionofff@gmail.com>
2024-10-22 12:58:32 +0500

    Update module_9_1.py

--- module_9_1.py
+++ module_9_1.py
@@ -1,8 +1,10 @@
 def apply_all_func(int_list: (int, float), *functions):
     results = {}
     for f in functions:
-        results[f.__name__] = (map(f, int_list))
+        results[f.__name__] = list(map(f, [int_list]))
+    print(*results.items())
     return results
+
 
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
