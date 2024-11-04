8a77c49ab5ddf5fa614d716c3ad1e61b117e5916
Galaktionofff <galaktionofff@gmail.com>
2024-11-04 17:57:22 +0500

    Update module_9_5.py

--- module_9_5.py
+++ module_9_5.py
@@ -13,12 +13,51 @@ class Iterator:
         return self
 
     def __next__(self):
-        if self.pointer > self.stop or self.pointer < self.stop:
-            raise StopIteration
-        else:
-            self.pointer += self.step
-            return self.pointer
+        x = 0
+        if self.step < 0:
+            if self.pointer < self.stop:
+                raise StopIteration
+            else:
+                x = self.pointer
+                self.pointer += self.step
+                return x
 
+        elif self.step > 0:
+            if self.pointer > self.stop:
+                raise StopIteration
+            else:
+                x = self.pointer
+                self.pointer += self.step
+                return x
+        else:
+            raise StopIteration
+
+
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
+
+iter2 = Iterator(-5, 1)
+iter3 = Iterator(6, 15, 2)
+iter4 = Iterator(5, 1, -1)
+iter5 = Iterator(10, 1)
+
+
+for i in iter2:
+    print(i, end=' ')
+    print()
+for i in iter3:
+    print(i, end=' ')
+    print()
+for i in iter4:
+    print(i, end=' ')
+    print()
+for i in iter5:
+    print(i, end=' ')
+    print()
 
 try:
     iter1 = Iterator(100, 200, 0)
