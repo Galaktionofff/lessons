97c71f8294ee99c2f10f96cafd7aee17aa6f7f43
Galaktionofff <galaktionofff@gmail.com>
2024-11-29 14:14:55 +0500

    Update module_11_2.py

--- module_11_2.py
+++ module_11_2.py
@@ -1,10 +1,15 @@
+import inspect as i
+from pprint import pprint
+
+
 def introspection_info(obj):
 
     all_attributes = dir(obj)
+
     return (
-        f'type: {type(obj)}, attributes: {[getattr(obj, attr) for attr in all_attributes if not callable(attr) and not attr.startswith("__")]},'
-        f' methods: {[name for name in all_attributes if i.ismethod(name) or i.isfunction(name)]}'
+        f'type: {type(obj)}, attributes: {[getattr(obj, attr) for attr in all_attributes if not callable(getattr(obj, attr)) and not attr.startswith("__")]},'
+        f' methods: {[attr for attr in all_attributes if callable(getattr(obj, attr)) and not attr.startswith("__")]}'
         f', module: {i.getmodule(obj)}')
 
-
+a = '123'
 pprint(introspection_info(a))
