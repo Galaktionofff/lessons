b5733bd2e6b476076baf529ae20f6e7d85df6dfc
Galaktionofff <galaktionofff@gmail.com>
2024-11-29 21:07:38 +0500

    Update module_12_1.py

--- module_12_1.py
+++ module_12_1.py
@@ -1,24 +1,17 @@
 from unittest import TestCase
-class Runner:
-    def __init__(self, name):
-        self.name = name
-        self.distance = 0
-
-    def run(self):
-        self.distance += 10
-
-    def walk(self):
-        self.distance += 5
-
-    def __str__(self):
-        return self.name
-
+from Ex import Runner
 class RunnerTest(TestCase):
     def test_walk(self):
         runner = Runner('Serega')
         for i in range(10):
             runner.walk()
         self.assertEquals(runner.distance, 50)
+
+    def test_run(self):
+        runner = Runner('Stepan')
+        for i in range(10):
+            runner.run()
+        self.assertEquals(runner.distance, 100)
 
     def test_challenge(self):
         runner1 = Runner('Ivan')
