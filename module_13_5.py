e770b85be33f4531eba0383efc5e727357795011
Galaktionofff <galaktionofff@gmail.com>
2024-12-01 00:28:09 +0500

    Create module_13_5.py

---
+++ module_13_5.py
@@ -0,0 +1,74 @@
+import aiogram as ai
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
+
+api = '7514207461:AAGu7ivIl_q3aDoo9-HQeRZEklGmNYjblD8'
+bot = ai.Bot(token=api)
+dp = ai.Dispatcher(bot, storage=MemoryStorage())
+
+"""Создание клавиатуры кнопок"""
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+button = KeyboardButton(text='Информация')
+button1 = KeyboardButton(text='Рассчитать')
+keyboard.add(button)
+keyboard.add(button1)
+
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=keyboard)
+
+
+@dp.message_handler(text='Информация')
+async def info(message):
+    await message.answer("Меня сделал Никитос")
+
+
+class User_state(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text='Рассчитать')
+async def set_age(message):
+    await message.answer('Введите свой возраст')
+    await User_state.age.set()
+
+
+@dp.message_handler(state=User_state.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer('Введите свой рост')
+    await User_state.growth.set()
+
+
+@dp.message_handler(state=User_state.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer('Введите свой вес')
+    await User_state.weight.set()
+
+
+@dp.message_handler(state=User_state.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+
+    await message.answer(f'Ваша норма калорий {10 * int(data["weight"]) + 6.25 * int(data["growth"]) - 5 * int(data["age"])}')
+    await state.finish()
+
+@dp.message_handler()
+async def all_message(message):
+    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+
+if __name__ == '__main__':
+    ai.executor.start_polling(dp, skip_updates=True)
