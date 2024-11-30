2bc56153e5eaea5fb540eb0a1b0498dbd2fddd8e
Galaktionofff <galaktionofff@gmail.com>
2024-11-30 10:40:23 +0500

    Update module_12_2.py

--- module_12_2.py
+++ module_12_2.py
@@ -14,26 +14,26 @@ class TestTournament(unittest.TestCase):
         self.runner1 = Ex.Runner('Андрей', 9)
         self.runner2 = Ex.Runner('Ник', 3)
 
-    @classmethod
-    def tearDownClass(cls):
-        pprint(cls.all_results)
+    def tearDown(self):
+        pprint(self.all_results)
 
     def test_speed_run(self):
         tour = Ex.Tournament(90, self.runner, self.runner2)
         t_s = tour.start()
-        self.all_results = t_s
+        self.all_results.update(t_s)
         self.assertTrue(self.all_results[2] == 'Ник')
 
     def test_speed_run1(self):
         tour = Ex.Tournament(90, self.runner1, self.runner2)
-        t_s = tour.start()
-        self.all_results = t_s
+        t_s = (tour.start())
+        self.all_results.update(t_s)
         self.assertTrue(self.all_results[2] == 'Ник')
 
     def test_speed_run2(self):
         tour = Ex.Tournament(90, self.runner, self.runner1, self.runner2)
         t_s = tour.start()
-        self.all_results = t_s
+        self.all_results.update(t_s)
         self.assertTrue(self.all_results[3] == 'Ник')
+
 
 test = TestTournament()
